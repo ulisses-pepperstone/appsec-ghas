ec46e1dfc4d5c84624c5dae1eee809a5936e414c
Ulisses Albuquerque <ulisses.albuquerque@pepperstone.com>
2025-04-08 10:20:01 +1000

    Add authentication endpoint

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 #!/usr/bin/env python
 
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET"])
 def root():
     return {"status": "ok"}
+
+@app.route("/auth", methods=["POST"])
+def authenticate():
+    payload = request.get_json()
+    if "username" in payload and "password" in payload:
+        if payload["username"] == "john.doe@domain.com":
+            if payload["password"] == "secret":
+                return {"status": "ok"}
+            else:
+                return {"status": "error", "message": "invalid password"}
+        else:
+            return {"status": "error", "message": "invalid username"}
+    return {"status": "error", "message": "invalid authentication request"}
 
 if __name__ == "__main__":
     app.run(debug=True)
