668434da17c9c436239217ef4aeec33d14ada632
Ulisses Albuquerque <ulisses.albuquerque@pepperstone.com>
2025-04-08 16:00:12 +1000

    Authentication flow, upload functionality

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 #!/usr/bin/env python
 
-import os.path
+import hashlib
+import http
+import os
+import peewee
+import uuid
 
 from flask import Flask, request
 
+# Database setup
+db = peewee.SqliteDatabase("appsec.sqlite")
+
+# Database models
+class User(peewee.Model):
+    id = peewee.UUIDField(primary_key=True)
+    email = peewee.CharField(unique=True)
+    password_hash = peewee.CharField(unique=True)
+    class Meta:
+        database = db
+
+# Flask setup
 app = Flask(__name__)
 
 
@@ -11,37 +27,69 @@ app = Flask(__name__)
 def root():
     return {"status": "ok"}
 
+@app.route("/register", methods=["POST"])
+def register():
+    try:
+        payload = request.get_json()
+        if "email" in payload and "password" in payload:
+            password_hash = hashlib.pbkdf2_hmac(
+                hash_name="sha256",
+                password=payload["password"].encode("utf-8"),
+                salt=payload["email"].encode("utf-8"),
+                iterations=1_000_000,
+            )
+            user = User.create(
+                id=uuid.uuid4(),
+                email=payload["email"],
+                password_hash=password_hash.hex()
+            )
+            return { "status": "ok", "user_id": user.get_id()}, http.HTTPStatus.OK
+        return { "status": "error", "reason": "invalid registration request" }, http.HTTPStatus.BAD_REQUEST
+    except Exception as err:
+        return {"status":"error", "reason": str(err)}, http.HTTPStatus.BAD_REQUEST
 
 @app.route("/auth", methods=["POST"])
 def authenticate():
-    payload = request.get_json()
-    if "username" in payload and "password" in payload:
-        if payload["username"] == "john.doe@domain.com":
-            if payload["password"] == "secret":
-                return {"status": "ok"}
-            else:
-                return {"status": "error", "message": "invalid password"}
-        else:
-            return {"status": "error", "message": "invalid username"}
-    return {"status": "error", "message": "invalid authentication request"}
-
-
-@app.route("/cart", methods=["GET"])
-def cart():
-    user = request.headers.get("x-user")
-    return {"user": user, "cart": []}
+    try:
+        payload = request.get_json()
+        if "email" in payload and "password" in payload:
+            password_hash = hashlib.pbkdf2_hmac(
+                hash_name="sha256",
+                password=payload["password"].encode("utf-8"),
+                salt=payload["email"].encode("utf-8"),
+                iterations=1_000_000,
+            )
+            user = User.select().where(
+                (User.email == payload["email"]) &
+                (User.password_hash == password_hash.hex())
+            ).get()
+            return { "status": "ok", "user_id": user.get_id()}, http.HTTPStatus.OK
+        return { "status": "error", "reason": "invalid registration request" }, http.HTTPStatus.UNAUTHORIZED
+    except Exception as err:
+        return {"status":"error", "reason": str(err)}, http.HTTPStatus.UNAUTHORIZED
 
 
 @app.route("/upload/<filename>", methods=["POST", "PUT"])
 def upload(filename: str):
-    output_path = os.path.join(os.path.dirname(__file__), filename)
-    f = request.files["the_file"]
-    f.save(output_path)
-    return {"status": "ok", "path": output_path}
+    try:
+        user_id = request.headers.get("x-user-id")
+        if user_id is None:
+            return {"status":"error", "reason": "user id required"}, http.HTTPStatus.UNAUTHORIZED
+        user = User.select().where(User.id == user_id).get()
+        output_path = os.path.join(os.path.dirname(__file__), user.email, filename)
+        if not os.path.exists(os.path.dirname(output_path)):
+            os.makedirs(name=os.path.dirname(output_path), mode=0o777)
+        data = request.get_data()
+        with open(output_path, "wb") as file_io:
+            file_io.write(data)
+        return {"status": "ok", "path": output_path}
+    except Exception as err:
+        return {"status":"error", "reason": str(err)}, http.HTTPStatus.UNAUTHORIZED
 
 
 if __name__ == "__main__":
-    import os
+    db.connect()
+    db.create_tables([User])
 
     debug_mode = os.getenv("FLASK_DEBUG", "False").lower() in ("true", "1", "t")
     app.run(debug=debug_mode)
