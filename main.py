97b71829fa68e880d86ee42689c0e888c1886bc2
Ulisses Albuquerque <ulisses.albuquerque@pepperstone.com>
2025-04-08 11:47:50 +1000

    Reformat code

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
 
+import os.path
+
 from flask import Flask, request
 
 app = Flask(__name__)
 
+
 @app.route("/", methods=["GET"])
 def root():
     return {"status": "ok"}
+
 
 @app.route("/auth", methods=["POST"])
 def authenticate():
@@ -21,19 +25,23 @@ def authenticate():
             return {"status": "error", "message": "invalid username"}
     return {"status": "error", "message": "invalid authentication request"}
 
+
 @app.route("/cart", methods=["GET"])
 def cart():
     user = request.headers.get("x-user")
     return {"user": user, "cart": []}
 
-@app.route("/upload/<filename>", methods=["POST","PUT"])
+
+@app.route("/upload/<filename>", methods=["POST", "PUT"])
 def upload(filename: str):
     output_path = os.path.join(os.path.dirname(__file__), filename)
     f = request.files["the_file"]
     f.save(output_path)
     return {"status": "ok", "path": output_path}
 
+
 if __name__ == "__main__":
     import os
+
     debug_mode = os.getenv("FLASK_DEBUG", "False").lower() in ("true", "1", "t")
     app.run(debug=debug_mode)
